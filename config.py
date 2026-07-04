f729c7468789b4cad794f186063c012cdb5ce567
ignore528 <aliakram7548@gmail.com>
2026-07-04 16:36:45 +0300

    Update config.py

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 # ======================================================
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/rockStar378/kirtipublic")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/ignore528/Ankitrepo")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv("GIT_TOKEN", "YOUR_GIT_TOKEN")
 
